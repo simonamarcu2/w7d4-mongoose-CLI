f7842db108eac83d2d27702c9bf5fb30fbe9dc4e
Simona Marcu <67797490+simonamarcu2@users.noreply.github.com>
2022-02-03 10:59:54 +0000

    created half application for films

---
+++ src/film/filmMethods.js
@@ -0,0 +1,3 @@
+const mongoose = require('mongoose');
+
+const FilmModel= require('./filmModel')
---
+++ src/film/filmModel.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+
+const filmSchema = mongoose.Schema({
+    name:{
+        type: String,
+        required:true,
+    },
+    actor:{
+        type: String,
+        default:'actor unknown',
+    }
+})
+
+const FilmModel = mongoose.model('Movies', filmSchema)
+model.exports = FilmModel
